4c3bd5eb7971f883cc9791471b6e888e8bdd7b00
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-05-16 15:52:28 -0300

    Update ex037.py

--- Exercicios/ex037.py
+++ Exercicios/ex037.py
@@ -5,3 +5,21 @@ escolher qual será a base de conversão:
 - 2 para octal
 - 3 para hexadecimal
 """
+
+n = int(input("Digite um número inteiro: "))
+
+print('''Escolha uma das bases para conversão:
+[ 1 ] - Converter para Binário
+[ 2 ] - Converter para Octal
+[ 3 ] - Converter para Hexadecimal''')
+
+opcao = int(input("Informe sua opção: "))
+
+if opcao == 1:
+    print(f'{n} convertido para Binário é igual a {bin(n)[2:]}')
+elif opcao == 2:
+    print(f'{n} convertido para Octal é igual a {oct(n)[2:]}')
+elif opcao	== 3:
+    print(f'{n} convertido para Hexadecimal é igual a {hex(n)[2:]}')
+else:
+    print('Opção inválida. Tente novamente.')

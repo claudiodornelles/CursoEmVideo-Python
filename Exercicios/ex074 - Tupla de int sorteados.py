7bcccdbc6748ab59b3bddcfaff8d2a0118f9aebe
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-01 23:42:15 -0300

    Update ex074 - Tupla de int sorteados.py

--- Exercicios/ex074 - Tupla de int sorteados.py
+++ Exercicios/ex074 - Tupla de int sorteados.py
@@ -4,20 +4,11 @@ Depois disso, mostre a listagem de números gerados e também indique o menor e 
 """
 from random import randint
 
-numeros = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+numeros = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
 
-maior = 0
-menor = 0
+print(f'Os valores sorteados foram: ', end='')
+for numero in numeros:
+    print(f'{numero} ', end='')
 
-for i in range(0, 5):
-    if i == 0:
-        maior = numeros[i]
-        menor = numeros[i]
-    if numeros[i] > maior:
-        maior = numeros[i]
-    if numeros[i] < menor:
-        menor = numeros[i]
-        
-print(f'Os valores sorteados foram: {numeros[0]} {numeros[1]} {numeros[2]} {numeros[3]} {numeros[4]}')
-print(f'O maior valor sorteado foi {maior}')
-print(f'O menor valor sorteador foi {menor}')
+print(f'\nO maior valor sorteado foi {max(numeros)}')
+print(f'O menor valor sorteador foi {min(numeros)}')

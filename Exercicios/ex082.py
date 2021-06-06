95dc9c865873cd6b83b0fe9e3085712e137ae343
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-06 14:16:35 -0300

    Update ex082.py

--- Exercicios/ex082.py
+++ Exercicios/ex082.py
@@ -5,15 +5,40 @@ Ao final, mostre o conteúdo das três listas geradas.
 """
 
 numeros = []
+pares = []
+impares = []
 
 while True:
     try:
         numero = int(input('Digite um valor: '))
 
     except:
+        print('Não entendi... ', end='')
         continue
     
     numeros.append(numero)
 
     while True:
-        resp = str(input('Deseja continuar? '))
+        resp = str(input('Deseja continuar? [S/N] '))
+        if resp in 'SsNn':
+            break
+        print('Não entendi... ', end='')
+    
+    if resp in 'Nn':
+        break
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+    else:
+        impares.append(numero)
+
+numeros.sort()
+pares.sort()
+impares.sort()
+
+print('-='*30)
+
+print(f'Os números digitados foram: {numeros}')
+print(f'Os números PARES digitados foram: {pares}')
+print(f'Os números ÍMPARES digitados foram: {impares}')

04676bd9786efacc849074dbb9ba0f4deb0e23d7
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-06 15:12:19 -0300

    Update ex080 - Lista ordenada automática.py

--- Exercicios/ex080 - Lista ordenada automática.py
+++ Exercicios/ex080 - Lista ordenada automática.py
@@ -4,27 +4,17 @@ Criar uma lista de números ordenada automáticamente
 
 """
 numeros = []
-adicionado = False
 
 for i in range(0,5):
     numero = int(input('Digite um valor: '))
-    adicionado = False
-    if i == 0:
+    if i == 0 or numero > numeros[-1]:
         numeros.append(numero)
-        adicionado = True
         print('Adicionado ao final da lista...')
     else:
         for posicao, valor in enumerate(numeros):
             if numero < valor:
                 numeros.insert(posicao, numero)
-                adicionado = True
                 print(f'Adicionado na posição {posicao} da lista...')
                 break
-        if not adicionado:
-            numeros.append(numero)
-            adicionado = True
-            print('Adicionado ao final da lista...')
 print('-='*30)
 print(f'Os valores digitaos em ordem foram {numeros}')
-#se o número for o maior, ele entra no final.
-#se não, entra no meio

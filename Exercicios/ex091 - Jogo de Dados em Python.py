f030ad8afc11fbd0b50dc76ba90d4aae7d1fbe5c
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-07 10:30:27 -0300

    Create ex091 - Jogo de Dados em Python.py

---
+++ Exercicios/ex091 - Jogo de Dados em Python.py
@@ -0,0 +1,44 @@
+"""
+Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
+"""
+
+from random import randint
+from time import sleep
+
+dicionario = {'Jogador 1':randint(1,6), 'Jogador 2':randint(1,6), 'Jogador 3':randint(1,6), 'Jogador 4':randint(1,6)}
+
+print('Valores sorteados:')
+sleep(0.75)
+for chave, valor in dicionario.items():
+    print(f'  O {chave} tirou {valor}')
+    sleep(0.75)
+
+print('Ranking dos jogadores:')
+
+dicionario_cp = dicionario.copy()
+ranking = []
+temp = []
+rep = 0
+
+for i in dicionario:
+    for chave, valor in dicionario_cp.items():
+
+        if rep == 0:
+            maior_valor = valor
+            maior_jogador = chave
+        elif valor > maior_valor:
+            maior_valor = valor
+            maior_jogador = chave
+        rep += 1
+
+    rep = 0
+    temp.append(maior_jogador)
+    temp.append(maior_valor)
+    ranking.append(temp[:])
+    temp.clear()
+    del dicionario_cp[maior_jogador]
+
+for pos, jogador in enumerate(ranking):
+    sleep(0.75)
+    print(f'  {pos + 1}º lugar: {jogador[0]} com {jogador[1]}')
+print()

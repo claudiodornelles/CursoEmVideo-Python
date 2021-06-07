5dfea0e9a498352381ec3587b314420e0efb38bf
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-06 22:24:01 -0300

    Create ex086 - Matriz em Python.py

---
+++ Exercicios/ex086 - Matriz em Python.py
@@ -0,0 +1,21 @@
+"""
+Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
+No final, mostre a matriz na tela com a formatação correta.
+"""
+
+matriz = [[],[],[]]
+
+for i in range(0,3):
+    for j in range(0,3):
+        n = int(input(f'Digite um valor para [{i}, {j}]: '))
+        matriz[i].append(n)
+
+print('-='*30)
+
+for i in range(0, 3):
+    for j in range(0,3):
+        if j != 2:
+            print(f'[ {matriz[i][j]} ]', end='')
+        else:
+            print(f'[ {matriz[i][j]} ]')
+print()

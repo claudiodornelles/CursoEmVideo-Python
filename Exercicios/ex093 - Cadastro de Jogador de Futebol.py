ae0eacf6d4906fd5977e75a6294947096a9d3f1c
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-08 12:07:01 -0300

    Update ex093 - Cadastro de Jogador de Futebol.py

--- Exercicios/ex093 - Cadastro de Jogador de Futebol.py
+++ Exercicios/ex093 - Cadastro de Jogador de Futebol.py
@@ -5,18 +5,15 @@ No final, tudo isso será guardado em um dicionário, incluíndo o total de gols
 
 jogador = {}
 partidas = []
-gols = total_gols = 0
 
 jogador['Nome'] = str(input('Nome do Jogador: ')).strip().title()
 tot_partidas = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
 
 for p in range(0, tot_partidas):
-    gols = int(input(f'Quantos gols {jogador["Nome"]} fez na partida {p + 1}? '))
-    partidas.append(gols)
-    total_gols += gols
+    partidas.append(int(input(f'Quantos gols {jogador["Nome"]} fez na partida {p + 1}? ')))
 
 jogador['Gols'] = partidas[:]
-jogador['Total'] = total_gols
+jogador['Total'] = sum(partidas)
 
 print('-='*30)
 print(jogador)
@@ -27,10 +24,10 @@ print('-='*30)
 print(f'O jogador {jogador["Nome"]} jogou {tot_partidas} partidas.')
 for np, ng in enumerate(partidas):
     if ng == 0:
-        print(f'=> Na partida {np + 1}, não fez nenhum gol.')
+        print(f'  => Na partida {np + 1}, não fez nenhum gol.')
     elif ng == 1:
-        print(f'=> Na partida {np + 1}, fez {ng} gol.')
+        print(f'  => Na partida {np + 1}, fez {ng} gol.')
     else:
-        print(f'=> Na partida {np + 1}, fez {ng} gols.')
-print(f'Foi um total de {total_gols} gols.')
+        print(f'  => Na partida {np + 1}, fez {ng} gols.')
+print(f'Foi um total de {jogador["Total"]} gols.')
 print()

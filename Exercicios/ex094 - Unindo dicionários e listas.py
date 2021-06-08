00d91dc559207bc9f5a072b47a4ad1c3704f1b56
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-08 12:17:09 -0300

    Update ex094 - Unindo dicionários e listas.py

--- Exercicios/ex094 - Unindo dicionários e listas.py
+++ Exercicios/ex094 - Unindo dicionários e listas.py
@@ -14,19 +14,19 @@ soma_idades = media = 0
 while True:
     temp['nome'] = str(input('Nome: ')).strip().title()
     while True:
-        temp['sexo'] = str(input('Sexo: [M/F] ')).upper().strip()
-        if temp['sexo'] in 'MFmf':
+        temp['sexo'] = str(input('Sexo: [M/F] ')).upper().strip()[0]
+        if temp['sexo'] in 'MF':
             break
         print('Erro! Por favor, digite apenas M ou F.')
     temp['idade'] = int(input('Idade: '))
     pessoas.append(temp.copy())
     temp.clear()
     while True:
-        resp = str(input('Quer continuar? [S/N] '))
-        if resp in 'SsNn':
+        resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+        if resp in 'SN':
             break
         print('Erro! Por favor, digite apenas S ou N.')
-    if resp in 'Nn':
+    if resp in 'N':
         break
 
 print('-='*30)
@@ -39,16 +39,19 @@ media = soma_idades / len(pessoas)
 
 print(f'- A média de idade é de {media:.0f} anos.')
 
-print(f'- As mulheres cadastradas foram: ', end='')
 for p in pessoas:
     if p['sexo'] == 'F':
         mulheres.append(p['nome'])
         mulheres.sort()
-for m in range(0, len(mulheres)):
-    if m < len(mulheres) - 1:
-        print(f'{mulheres[m]}, ', end='')
-    else:
-        print(f'\b\b e {mulheres[m]}.')
+if len(mulheres) > 0:
+    print(f'- As mulheres cadastradas foram: ', end='')
+    for m in range(0, len(mulheres)):
+        if m < len(mulheres) - 1:
+            print(f'{mulheres[m]}, ', end='')
+        else:
+            print(f'\b\b e {mulheres[m]}.')
+else:
+    print('- Não foram cadastradas mulheres na lista.')
 
 print('- Lista das pessoas com idade acima da média:')
 for pessoa in pessoas:

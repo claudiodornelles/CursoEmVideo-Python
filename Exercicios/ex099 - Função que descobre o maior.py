4f60b84a0b596c2b0fa93ee732333708a7f41582
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-08 16:12:28 -0300

    Create ex099 - Função que descobre o maior.py

---
+++ Exercicios/ex099 - Função que descobre o maior.py
@@ -0,0 +1,28 @@
+"""
+Faça um programa que tenha uma função chamada maior() que receba vários parâmetros com valores inteiros.
+Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+"""
+from time import sleep
+
+def maior(* numeros):
+    print('-='*30)
+    print('Analisando os valores passados...')
+    if len(numeros) != 0:
+        maior = numeros[0]
+        for numero in numeros:
+            if numero > maior:
+                maior = numero
+            print(f'{numero} ', end='', flush=True)
+            sleep(.5)
+        print()
+    else:
+        maior = 0
+    print(f'Foram informados {len(numeros)} valores ao todo.')
+    print(f'O maior valor informado foi {maior}')
+
+
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()

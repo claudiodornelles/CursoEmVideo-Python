ed5c1885476ad1207264c8cf993d737934f45605
Claudio Dornelles <82041667+claudiodornelles@users.noreply.github.com>
2021-06-08 15:41:53 -0300

    Create ex097 - Um print especial.py

---
+++ Exercicios/ex097 - Um print especial.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que tenha a função chamada escreva() que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+Ex:
+escreva('Olá, Mundo!')
+Saída:
+~~~~~~~~~~~~~
+ Olá, Mundo!
+~~~~~~~~~~~~~
+"""
+
+def escreva(txt):
+    tam = len(txt) + 4
+    print('~'*tam)
+    print(f'  {txt}  ')
+    print('~'*tam)
+
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
